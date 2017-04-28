a45661e0f573ed9ee21a9eead5985ae33739bf7f
Administrator <Administrator@SC-201702281216>
2017-04-28 15:22:26 +0800

    Implemented Histogram Equalization

--- src/FXCV/FXCVController.java
+++ src/FXCV/FXCVController.java
@@ -1,15 +1,24 @@
 package FXCV;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfFloat;
+import org.opencv.core.MatOfInt;
+import org.opencv.core.Point;
+import org.opencv.core.Scalar;
 import org.opencv.imgproc.Imgproc;
 import org.opencv.videoio.VideoCapture;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -29,7 +38,16 @@ public class FXCVController
 {
 	// the FXML button
 	@FXML
-	private Button button;
+	private Button start_btn;
+	// the FXML grayscale checkbox
+	@FXML
+	private CheckBox grayscale;
+	// the FXML histogram equalizer checkbox
+	@FXML
+	private CheckBox histeq;
+	// the FXML histogram view
+	@FXML
+	private ImageView histogram;
 	// the FXML image view
 	@FXML
 	private ImageView currentFrame;
@@ -80,7 +98,7 @@ public class FXCVController
 				this.timer.scheduleAtFixedRate(frameGrabber, 0, 33, TimeUnit.MILLISECONDS);
 				
 				// update the button content
-				this.button.setText("Stop Camera");
+				this.start_btn.setText("Stop Camera");
 			}
 			else
 			{
@@ -93,7 +111,7 @@ public class FXCVController
 			// the camera is not active at this point
 			this.cameraActive = false;
 			// update again the button content
-			this.button.setText("Start Camera");
+			this.start_btn.setText("Start Camera");
 			
 			// stop the timer
 			this.stopAcquisition();
@@ -121,7 +139,19 @@ public class FXCVController
 				// if the frame is not empty, process it
 				if (!frame.empty())
 				{
-					Imgproc.cvtColor(frame, frame, Imgproc.COLOR_BGR2GRAY);
+					if (grayscale.isSelected())
+					{
+					   Imgproc.cvtColor(frame, frame, Imgproc.COLOR_BGR2GRAY);
+					} 
+					
+					if (histeq.isSelected())
+					{
+						this.HistEq(frame, grayscale.isSelected());
+					}
+					
+					// show the histogram
+					this.showHistogram(frame, grayscale.isSelected());
+					
 				}
 				
 			}
@@ -133,6 +163,107 @@ public class FXCVController
 		}
 		
 		return frame;
+	}
+	
+	/**
+	 * Compute and show the histogram for the given {@link Mat} image
+	 * 
+	 * @param frame
+	 *            the {@link Mat} image for which compute the histogram
+	 * @param gray
+	 *            is a grayscale image?
+	 */
+	private void showHistogram(Mat frame, boolean gray)
+	{
+		// split the frames in multiple images
+		List<Mat> images = new ArrayList<Mat>();
+		Core.split(frame, images);
+		
+		// set the number of bins at 256
+		MatOfInt histSize = new MatOfInt(256);
+		// only one channel
+		MatOfInt channels = new MatOfInt(0);
+		// set the ranges
+		MatOfFloat histRange = new MatOfFloat(0, 256);
+		
+		// compute the histograms for the B, G and R components
+		Mat hist_b = new Mat();
+		Mat hist_g = new Mat();
+		Mat hist_r = new Mat();
+		
+		// B component or gray image
+		Imgproc.calcHist(images.subList(0, 1), channels, new Mat(), hist_b, histSize, histRange, false);
+		
+		// G and R components (if the image is not in gray scale)
+		if (!gray)
+		{
+			Imgproc.calcHist(images.subList(1, 2), channels, new Mat(), hist_g, histSize, histRange, false);
+			Imgproc.calcHist(images.subList(2, 3), channels, new Mat(), hist_r, histSize, histRange, false);
+		}
+		
+		// draw the histogram
+		int hist_w = 150; // width of the histogram image
+		int hist_h = 150; // height of the histogram image
+		int bin_w = (int) Math.round(hist_w / histSize.get(0, 0)[0]);
+		
+		Mat histImage = new Mat(hist_h, hist_w, CvType.CV_8UC3, new Scalar(0, 0, 0));
+		// normalize the result to [0, histImage.rows()]
+		Core.normalize(hist_b, hist_b, 0, histImage.rows(), Core.NORM_MINMAX, -1, new Mat());
+		
+		// for G and R components
+		if (!gray)
+		{
+			Core.normalize(hist_g, hist_g, 0, histImage.rows(), Core.NORM_MINMAX, -1, new Mat());
+			Core.normalize(hist_r, hist_r, 0, histImage.rows(), Core.NORM_MINMAX, -1, new Mat());
+		}
+		
+		// effectively draw the histogram(s)
+		for (int i = 1; i < histSize.get(0, 0)[0]; i++)
+		{
+			// B component or gray image
+			Imgproc.line(histImage, new Point(bin_w * (i - 1), hist_h - Math.round(hist_b.get(i - 1, 0)[0])),
+					new Point(bin_w * (i), hist_h - Math.round(hist_b.get(i, 0)[0])), new Scalar(255, 0, 0), 2, 8, 0);
+			// G and R components (if the image is not in gray scale)
+			if (!gray)
+			{
+				Imgproc.line(histImage, new Point(bin_w * (i - 1), hist_h - Math.round(hist_g.get(i - 1, 0)[0])),
+						new Point(bin_w * (i), hist_h - Math.round(hist_g.get(i, 0)[0])), new Scalar(0, 255, 0), 2, 8,
+						0);
+				Imgproc.line(histImage, new Point(bin_w * (i - 1), hist_h - Math.round(hist_r.get(i - 1, 0)[0])),
+						new Point(bin_w * (i), hist_h - Math.round(hist_r.get(i, 0)[0])), new Scalar(0, 0, 255), 2, 8,
+						0);
+			}
+		}
+		
+		// display the histogram...
+		Image histImg = Utils.mat2Image(histImage);
+		updateImageView(histogram, histImg);
+		
+	}
+	
+	/**
+	 * Equalize histogram for the given {@link Mat} image
+	 * 
+	 * @param frame
+	 *            the {@link Mat} image for which compute the histogram
+	 * @param gray
+	 *            is a grayscale image?
+	 */
+	private void HistEq(Mat frame, boolean gray)
+	{
+		// split the frames in multiple images
+		List<Mat> hsv = new ArrayList<Mat>();
+		
+		if (!gray)
+		{
+			Imgproc.cvtColor(frame, frame, Imgproc.COLOR_BGR2HSV);
+			Core.split(frame, hsv);
+			Imgproc.equalizeHist(hsv.get(2), hsv.get(2));
+			Core.merge(hsv, frame);
+			Imgproc.cvtColor(frame, frame, Imgproc.COLOR_HSV2BGR);
+		} else {
+			Imgproc.equalizeHist(frame, frame);
+		}		
 	}
 	
 	/**
